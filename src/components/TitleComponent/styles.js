79e43d068045fe95079c83a80726b85d6fc47dc7
Haidon Shultz <haidonshultz@gmail.com>
2020-03-26 19:05:30 -0700

    Initialized TitleComponent

---
+++ src/components/TitleComponent/styles.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+import {colors} from '../../styles';
+
+export default StyleSheet.create({
+  appTitle: {
+    fontFamily: 'Apple SD Gothic Neo',
+    textAlign: 'left',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    padding: 20,
+    fontSize: 50,
+    color: colors.black,
+    marginTop: 40,
+    marginBottom: 14,
+  },
+  dividerContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  dividerLine: {
+    flex: 1,
+    backgroundColor: colors.black,
+    height: 1,
+  },
+  dividerText: {
+    fontFamily: 'Apple SD Gothic Neo',
+    textAlign: 'center',
+    paddingHorizontal: 12,
+    fontSize: 18,
+    color: colors.black,
+  },
+});
