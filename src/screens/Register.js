2a07ee51a3585437e8bb6508d28d9f568d17feab
Haidon Shultz <haidonshultz@gmail.com>
2020-03-31 13:29:10 -0700

    bugfix

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -188,6 +188,18 @@ export default class RegisterScreen extends React.Component {
     });
   }
 
+  onChangeSex(sex) {
+    this.setState({
+      sex: sex,
+    });
+  }
+
+  onChangeDob(dob) {
+    this.setState({
+      dob: dob,
+    });
+  }
+
   validateEmail(email) {
     const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
 
@@ -259,6 +271,7 @@ export default class RegisterScreen extends React.Component {
               choices={["Male", "Female", "Other", "Prefer not to say"]}
               default="Sex"
               title='Sex'
+              onSelect={selected => this.onChangeSex(selected)}
             >
             </DropdownComponent>
             <View style={styles.inlineFiller} />
@@ -266,8 +279,8 @@ export default class RegisterScreen extends React.Component {
               title='Date of Birth'
               placeholderText='1/25/1998'
               autoCapitalize="words"
-              onChangeText={lastName => this.onChangeLastName(lastName)}
-              value={this.state.lastName}
+              onChangeText={dob => this.onChangeDob(dob)}
+              value={this.state.dob}
               >
             </TextInputComponent>
           </View>
