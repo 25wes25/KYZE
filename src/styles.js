45728929b1a67b9b4b8dc3a58e9dcc59fb0dc095
Wesley Swanson <wesleyswanson97@gmail.com>
2020-04-03 18:48:58 -0700

    Polished tabs

--- src/styles.js
+++ src/styles.js
@@ -2,6 +2,7 @@ export const colors = {
   white: '#ffffff',
   black: '#000000',
   lightGray: '#BDBDBD',
+  mediumGray: '#666666',
   darkGray: '#222222',
   opaqueBlack: 'rgba(0, 0, 0, 0.4)',
   mintGreen: '#3D9690',
