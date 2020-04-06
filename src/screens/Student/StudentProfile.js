fb383d4a8f00360a2b7bef1bc5ae9d991bd927fd
Haidon Shultz <haidonshultz@gmail.com>
2020-04-05 18:12:41 -0700

    bugfix

--- src/screens/Student/StudentProfile.js
+++ src/screens/Student/StudentProfile.js
@@ -8,8 +8,9 @@ import {
   Image,
   View,
   TouchableOpacity,
+  FlatList,
 } from 'react-native';
-import {colors} from '../../styles';
+import {colors, fonts} from '../../styles';
 
 const user = {
   name: 'Pedram H.',
@@ -21,7 +22,76 @@ const shortcuts = [
   "Santa Monica Library",
 ];
 
+let sessionsData = [
+  {
+    id: '0',
+    duration: 2,
+    earnings: 60,
+    subject: 'Organic Chemistry I',
+    date: 'April 3, 2020',
+  },
+  {
+    id: '1',
+    duration: 1,
+    earnings: 30,
+    subject: 'Algebra I',
+    date: 'March 29, 2020',
+  },
+  {
+    id: '2',
+    duration: 2,
+    earnings: 40,
+    subject: 'Calculus II',
+    date: 'March 24, 2020',
+  },
+  {
+    id: '3',
+    duration: 2,
+    earnings: 60,
+    subject: 'Organic Chemistry I',
+    date: 'March 23, 2020',
+  },
+  {
+    id: '4',
+    duration: 1,
+    earnings: 30,
+    subject: 'Algebra I',
+    date: 'March 20, 2020',
+  },
+  {
+    id: '5',
+    duration: 2,
+    earnings: 40,
+    subject: 'Calculus II',
+    date: 'March 16, 2020',
+  },
+];
+
 export default class StudentProfileScreen extends React.Component {
+  renderItem(item) {
+    return (
+      <TouchableOpacity style={styles.sessionButton}>
+        <View style={styles.sessionContainer}>
+          <View style={styles.sessionIcon} />
+          <View style={styles.sessionDetailsContainer}>
+            <Text style={styles.sessionSubjectText}>{item.subject}</Text>
+            <Text style={styles.sessionDetailsText}>
+              {item.date} * {item.duration} hrs
+            </Text>
+          </View>
+          <View style={styles.sessionEarningsContainer}>
+            <Text style={styles.sessionEarnings}>${item.earnings}.00</Text>
+            <Image
+              style={styles.nextArrow}
+              source={require('../../../res/images/rightArrow.png')}
+            />
+          </View>
+        </View>
+        <View style={styles.dividerLine} />
+      </TouchableOpacity>
+    );
+  }
+
   render() {
     return (
       <SafeAreaView style={styles.container}>
@@ -71,27 +141,12 @@ export default class StudentProfileScreen extends React.Component {
             <Text style={styles.heading}>
               Session History
             </Text>
-            {shortcuts.map((e, i) => {
-                return (
-                  <View key={i}>
-                    <View style={styles.dividerLine}/>
-                    <TouchableOpacity>
-                      <View style={{
-                        flex: 1,
-                        flexDirection: 'row',
-                        alignItems: 'center',
-                        marginVertical: 10,
-                      }}>
-                        <View style={styles.icon}/>
-                        <Text style={styles.heading}>
-                          {e}
-                        </Text>
-                      </View>
-                    </TouchableOpacity>
-                  </View>
-                );
-              })
-            }
+            <View style={styles.dividerLine}/>
+            <FlatList
+              data={sessionsData}
+              renderItem={({item}) => this.renderItem(item)}
+              keyExtractor={item => item.id}
+            />
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -156,5 +211,47 @@ const styles = StyleSheet.create({
     color: colors.black,
     textAlign: 'center',
     marginBottom: 5,
+  },
+  earningsHistoryList: {
+    flex: 1,
+  },
+  sessionButton: {},
+  sessionContainer: {
+    flexDirection: 'row',
+    paddingVertical: 10,
+  },
+  sessionIcon: {
+    height: 32,
+    width: 32,
+    borderRadius: 16,
+    backgroundColor: colors.lightGray,
+  },
+  sessionDetailsContainer: {
+    flex: 1,
+    paddingLeft: 10,
+  },
+  sessionSubjectText: {
+    fontFamily: fonts.gothic,
+    fontSize: 16,
+    fontWeight: '800',
+  },
+  sessionDetailsText: {
+    fontFamily: fonts.gothic,
+    fontSize: 12,
+    fontWeight: '500',
+  },
+  sessionEarningsContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  sessionEarnings: {
+    fontFamily: fonts.gothic,
+    fontSize: 16,
+    fontWeight: '500',
+    paddingTop: 5,
+  },
+  nextArrow: {
+    marginLeft: 10,
   },
 });
